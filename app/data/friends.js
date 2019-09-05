e98156d5a8d909e2d0cdcbec0e883dbf063613bb
Sunny <loganayers2013@yahoo.com>
2019-09-05 18:22:14 -0500

    deploying to heroku

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,6 +4,6 @@ friends = [
         photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjgv8Dsp7rkAhVCgK0KHdxJB10QjRx6BAgBEAQ&url=https%3A%2F%2Fleonardodicaprio.com%2F&psig=AOvVaw132fx5VC3oxmPeALuIl5h1&ust=1567794516576549",
         scores: [5, 1, 2, 5, 3, 4, 2, 3, 1, 2]
     }
-]
+];
 
 module.exports = friends;
