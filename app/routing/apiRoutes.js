cbc8b34793f5c16e7b76590143ed82e4f4921bd0
Sunny <loganayers2013@yahoo.com>
2019-11-08 12:33:44 -0600

    prepared for deployment

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,29 +5,30 @@ module.exports = function (app) {
     app.get("/api/friends", function (req, res) {
         res.json(friends);
     })
+
+
+    app.post("/api/friends", function (req, res) {
+
+        var user = req.body;
+        //console.log(user);
+
+        var newFriend = {};
+        var topCompatibility = 50;
+        friends.forEach(function (friends) {
+            var compatibility = 0;
+            for (var i = 0; i < friends.length; i++) {
+                user.scores[i] = parseInt(user.scores[i]);
+                compatibility += (user.scores[i] - friends.scores[i]);
+            }
+            if (compatibility < topCompatibility) {
+                topCompatibility = compatibility;
+                newFriend = friends;
+            }
+        });
+        res.json({
+            name: newFriend.name,
+            photo: newFriend.photo
+        });
+        friends.push(user);
+    })
 }
-
-app.post("/api/friends", function (req, res) {
-
-    var user = req.body;
-    //console.log(user);
-
-    var newFriend = {};
-    var topCompatibility = 50;
-    friends.forEach(function (friends) {
-        var compatibility = 0;
-        for (var i = 0; i < friends.length; i++) {
-            user.scores[i] = parseInt(user.scores[i]);
-            compatibility += (user.scores[i] - friends.scores[i]);
-        }
-        if (compatibility < topCompatibility) {
-            topCompatibility = compatibility;
-            newFriend = friends;
-        }
-    });
-    res.json({
-        name: newFriend.name,
-        photo: newFriend.photo
-    });
-    friends.push(user);
-})
